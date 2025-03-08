6679be3c2b15f44171ccab3ed8ae838d77be3968
Ian Lee <ianlee.tyl@gmail.com>
2025-03-07 23:59:42 -0500

    Add some writeups

--- app/page.tsx
+++ app/page.tsx
@@ -44,18 +44,48 @@ export default function Page() {
       </p>
 
       <div className="big">
-        Persuative Visualization
+        Proponent's Perspective
       </div>
       <hr className="solid-line"></hr>
       <CO2Slider></CO2Slider>
       <figcaption className="text">
         [Figure 1] 
-        Percent increase in CO2 concentration of our atmosphere using year 1958 (the first recorded year in the IMF dataset) as a comparison baseline,
-        As we slide left to right, we observe a clear rise in CO2 level over the years (1958-2024).
+        Percent increase in atmospheric CO2 concentration from year 1958-2024, using 1958 as the comparison baseline (the first recorded year in the IMF dataset). Interact with the slider to observe change.
       </figcaption>
+      <h3 className='text-space'>Design Decisions:</h3>
+      <ul className="normal">
+            <li>
+              <span className="u">Data Transformation to Aggregate By Year</span> Score: 2
+              <ul>
+                <li>Score: 2 (Fully Earnest)</li>
+                <li>
+                  Rationale: The original dataset records the concentration data in months. This makes it challenging to observe macro-trends that tend to be more appealing to the general audience, since climate change is an event that has more noticeble effect over a longer-horizon.
+
+                </li>
+              </ul>
+            </li>
+
+            <li>
+              <span className="u">Dynamic Color Encoding </span>
+              <ul>
+                <li>Score: 2 (Fully Earnest)</li>
+                <li>Score: 2</li>
+              </ul>
+            </li>
+
+            <li>
+              <span className="u">Interactive Slider From Left to Right </span> Score: 2
+            </li>
+
+            <li>
+              <span className="u">Relate to Readers with Emojis </span> Score: 0
+            </li>
+
+        </ul>
+
 
       <div className="big">
-        Deceptive Visualization
+        Opponent's Perspective
       </div>
       <hr className="solid-line"></hr>
 
